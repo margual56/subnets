605741d8e431848e5eb92637f99fb2b05777bfa4
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2022-11-10 17:59:19 +0100

    Added clap and basic cli use

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
-#[macro_use]
-extern crate text_io;
-
+use clap::Parser;
 use subnets::ip::IP;
 
-fn main() {
-    // reads until a \n is encountered
-    let line: &str = "192.168.0.15/16"; //read!("{}\n");
+#[derive(Parser, Debug)]
+#[command(author, version, about,
+    long_about = "Note: When playing, all the keybindings of mpv can be used, and `q` is reserved for exiting the program"
+)]
+pub struct Cli {
+    /// IP address (with or without a mask)
+    ip: String,
 
-    match IP::from_str(line) {
-        Err(e) => println!("{:}", e),
-        Ok(i) => {
-            println!("{}", i);
-            println!("{:?}", i);
-        }
+    /// Option: -u --url <URL>: Specifies an url to be played.
+    #[clap(short, long, help = "Specifies an url to be played.")]
+    url: Option<String>,
+
+    /// Option: -s --station <station name>: Specifies the name of the station to be played
+    #[clap(
+        short,
+        long,
+        conflicts_with = "url",
+        help = "Specifies the name of the station to be played."
+    )]
+    station: Option<String>,
+}
+
+fn main() {
+    let args = Cli::parse();
+    
+    // reads until a \n is encountered
+    let Ok(ip) = IP::from_str(&args.ip) else {
+        panic!("The IP is incorrect");
     };
+
+    println!("IP: {}", ip);
+    println!("The mask is {} and can hold {} PCs (+1 gateway)", ip.mask, ip.get_hosts());
+    let range = ip.get_range();
+    println!("With this mask, the subnet IP range is from {} to {}", range.0, range.1);
+    println!("IP in binary: {:?}", ip.ip);
+    println!("Mask in binary: {:?}", ip.mask);
 }
