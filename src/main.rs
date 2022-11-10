f1224c4795f28f75300ec4edfe05cc24e4b0ad01
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2022-11-10 19:28:08 +0100

    Added colors, moved summary to struct Also removed Result from the generation methods of the IP struct due to them being redundant

--- src/main.rs
+++ src/main.rs
@@ -12,39 +12,24 @@ pub struct Cli {
     /// IP address (with or without a mask)
     ip: String,
 
-    /// Option: -u --url <URL>: Specifies an url to be played.
-    #[clap(short, long, help = "Specifies an url to be played.")]
-    url: Option<String>,
+    /// Option: -n --hosts <number of hosts>: Specifies a number of hosts to calculate a mask.
+    #[clap(short = 'n', long, help = "Specifies the number of hosts.")]
+    hosts: Option<u32>,
 
     /// Option: -s --station <station name>: Specifies the name of the station to be played
-    #[clap(
-        short,
-        long,
-        conflicts_with = "url",
-        help = "Specifies the name of the station to be played."
-    )]
+    #[clap(short, long, help = "Specifies the name of the station to be played.")]
     station: Option<String>,
 }
 
 fn main() {
     let args = Cli::parse();
 
-    // reads until a \n is encountered
-    let Ok(ip) = IP::from_str(&args.ip) else {
-        panic!("The IP is incorrect");
+    let ip: IP = match args.hosts {
+        None => IP::from_str(&args.ip),
+        Some(hosts) => IP::from_hosts(&args.ip, hosts),
     };
 
-    println!("IP: {}", ip);
-    println!(
-        "The mask is {} and can hold {} PCs (+1 gateway)",
-        ip.mask,
-        ip.get_hosts()
-    );
-    let range = ip.get_range();
-    println!(
-        "With this mask, the subnet IP range is from {} to {}",
-        range.0, range.1
-    );
-    println!("IP in binary: {:?}", ip.ip);
-    println!("Mask in binary: {:?}", ip.mask);
+    println!("{}", ip.summary());
+
+    // reads until a \n is encountered
 }
